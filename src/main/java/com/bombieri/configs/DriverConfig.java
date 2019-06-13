9e9c929aeb48627bcb5c793f84f63fa52c026dd2
Bombieri5 <Bombieri5@Bombieri-WebDev1>
2019-06-13 11:56:40 -0300

    segundoCommit

---
+++ src/main/java/com/bombieri/configs/DriverConfig.java
@@ -0,0 +1,25 @@
+package com.bombieri.configs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverConfig {
+	String url = System.getProperty("url");
+	String browser = System.getProperty("browser");
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public WebDriver getDriver() {
+		WebDriver driver = null;
+		if (this.browser == "chrome") {
+			System.getProperties().setProperty("webdriver.chrome.driver", "C:\\Users\\Bombieri5\\Downloads\\Instaladores\\chromedriver_win32_75\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		return driver;
+	}
+}
