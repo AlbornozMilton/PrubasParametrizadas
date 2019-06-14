54caa6a4ee840b7eb8d7ee47b4df096446b5c803
Bombieri5 <Bombieri5@Bombieri-WebDev1>
2019-06-14 11:27:11 -0300

    opcion ejecucion Firefox

--- src/main/java/com/bombieri/configs/DriverConfig.java
+++ src/main/java/com/bombieri/configs/DriverConfig.java
@@ -2,6 +2,7 @@ package com.bombieri.configs;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverConfig {
 	String url = System.getProperty("urlValue");
@@ -23,6 +24,10 @@ public class DriverConfig {
 		if (this.browser.equals("chrome")) {
 			System.getProperties().setProperty("webdriver.chrome.driver", "C:\\Users\\Bombieri5\\Downloads\\Instaladores\\chromedriver_win32_75\\chromedriver.exe");
 			driver = new ChromeDriver();
+		}
+		if (this.browser.equals("firefox")) {
+			System.getProperties().setProperty("geckodriver.firefox.driver", "C:\\Users\\Bombieri5\\Downloads\\Instaladores\\firefoxdriver-win32_55\\geckodriver.exe" );
+			driver = new FirefoxDriver();
 		}
 		return driver;
 	}
