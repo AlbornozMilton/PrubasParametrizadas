789c30d7350fb6d209a16c17827b800995b5a00a
Bombieri5 <Bombieri5@Bombieri-WebDev1>
2019-06-18 11:56:14 -0300

    actualizacion prueba

--- src/test/java/com/bombieri/tests/BombieriTest.java
+++ src/test/java/com/bombieri/tests/BombieriTest.java
@@ -6,6 +6,7 @@ import com.bombieri.configs.DriverConfig;
 
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
@@ -19,9 +20,8 @@ public class BombieriTest {
 	@Test
 	public void f() {
 		driver.get(url);
-		  WebElement element = driver.findElement(By.name("q"));
-		  element.sendKeys("bombieri software");
-		  element.submit();
+		  driver.findElement(By.name("q")).sendKeys("bombieri software");
+		  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 		  driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/div/div/div[1]/a")).click();
 		  driver.findElement(By.xpath("//a[contains(@href,'#')]")).click();
 		  driver.findElement(By.xpath("//a[contains(@href,'https://www.bombieri.com.ar/b/consulting')]")).click();
