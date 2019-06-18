9a6029a2d559998785689a7bf700899c0647aaac
Bombieri5 <Bombieri5@Bombieri-WebDev1>
2019-06-18 11:59:38 -0300

    agregado en testng las pruebas

--- src/test/java/com/bombieri/tests/BombieriTest.java
+++ src/test/java/com/bombieri/tests/BombieriTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
 
 public class BombieriTest {
