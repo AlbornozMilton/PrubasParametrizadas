0323ea9000e46ee76deac9ebc98cf0e603e359f8
Bombieri5 <Bombieri5@Bombieri-WebDev1>
2019-06-21 08:56:55 -0300

    fix error con firefox

--- src/test/java/com/bombieri/tests/BombieriTest.java
+++ src/test/java/com/bombieri/tests/BombieriTest.java
@@ -8,6 +8,8 @@ import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class BombieriTest {
@@ -17,10 +19,13 @@ public class BombieriTest {
 	String url = dConfig.getUrl();
 
 	@Test
-	public void f() {
+	public void f() throws InterruptedException {
 		driver.get(url);
 		  driver.findElement(By.name("q")).sendKeys("bombieri software");
 		  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		  
+		  WebDriverWait wait = new WebDriverWait(driver,120);
+		  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='rso']/div[1]/div/div/div/div/div[1]/a")));
 		  driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/div/div/div[1]/a")).click();
 		  driver.findElement(By.xpath("//a[contains(@href,'#')]")).click();
 		  driver.findElement(By.xpath("//a[contains(@href,'https://www.bombieri.com.ar/b/consulting')]")).click();
